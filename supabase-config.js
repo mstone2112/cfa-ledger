169c2eae004491ce5c6f04b7d5eafa90c1bcc690
mstone2112 <mstone2112@gmail.com>
2026-07-06 09:12:19 -0700

    Update SUPABASE_URL to remove '/rest/v1/' suffix

--- supabase-config.js
+++ supabase-config.js
@@ -3,5 +3,5 @@
 // See README.md, section "3. Connect the app to Supabase" for
 // exactly where to find these two values in the Supabase dashboard.
 // ============================================================
-window.SUPABASE_URL = "https://cxfkjriockzbqstdbrdy.supabase.co/rest/v1/";
+window.SUPABASE_URL = "https://cxfkjriockzbqstdbrdy.supabase.co";
 window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN4ZmtqcmlvY2t6YnFzdGRicmR5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMzMDAwNDgsImV4cCI6MjA5ODg3NjA0OH0.Uzk_6YKsxgmWEerQilM2EePsU1yL0UM0eGdKwwNYMso";
